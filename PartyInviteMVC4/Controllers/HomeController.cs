34bd660f2b7f02af84d3d5dc8d9758034530be4b
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2016-01-16 08:34:20 +0530

    Initial commit

---
+++ PartyInviteMVC4/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInviteMVC4.Models;
+
+namespace PartyInviteMVC4.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Title = "Index";
+            ViewBag.Greeting = hour < 12 ?  "Good Morning" : "Good Afternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RSVPForm()
+        {
+            ViewBag.Title = "RSVP Form";
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RSVPForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Title = "Thanks";
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
---
+++ PartyInviteMVC4/Models/GuestResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInviteMVC4.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter the name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter the email address.")]
+        [RegularExpression(".\\@.+\\..+",
+            ErrorMessage = "Please enter the valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you will attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
